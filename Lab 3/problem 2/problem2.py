1b75cc8e4bb279e629416b3c0a826fad3ecc823c
PotapovAnton-gif <antpotapov2019@gmail.com>
2020-11-26 20:14:32 +0300

    part of problem2

--- Lab 3/problem 2/problem2.py
+++ Lab 3/problem 2/problem2.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
-import ma
+import matplotlib.pyplot as plt
 
 data = pd.read_csv(r"C:\Users\antho\Labs\Lab 3\problem 2\flights.csv")
 
 Nimble = data[lambda x: x['CARGO'] == 'Nimble']
 Medium = data[lambda x: x['CARGO'] == 'Medium']
 Jumbo = data[lambda x: x['CARGO'] == 'Jumbo']
+
+plt.hist()
 
