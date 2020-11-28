aeddbbf3913c536baabc16621df51a25eeadc589
PotapovAnton-gif <antpotapov2019@gmail.com>
2020-11-29 00:34:20 +0300

    .

--- Lab 3/problem 3/problem3.py
+++ Lab 3/problem 3/problem3.py
@@ -1,9 +1,17 @@
 import numpy as np
 import pandas as pd
+import requests
+from functools import reduce
 
-data1 = pd.read_csv(r"C:\Users\antho\Labs\Lab 3\problem 3\students_info.csv")
-data2 = pd.read_html(r"C:\Users\antho\Labs\Lab 3\problem 3\results_ejudge.html")
+data1 = pd.read_html("/home/antpotapov2019/labs_for_py/Lab 3/problem 3/results_ejudge.html")
+data2 = pd.read_excel("/home/antpotapov2019/labs_for_py/Lab 3/problem 3/students_info.xlsx")
 
-print(data2)
+data2.rename(columns= {'login':'User'}, inplace=True)
+
+
+data1.
+print(data1)
+
+
 
 
