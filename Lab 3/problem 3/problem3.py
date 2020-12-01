029f53281e82ce4241c171e1b42835eec533a711
PotapovAnton-gif <antpotapov2019@gmail.com>
2020-12-01 19:51:55 +0300

    probelm 1, Lab 4 - done

--- Lab 3/problem 3/problem3.py
+++ Lab 3/problem 3/problem3.py
@@ -1,16 +1,17 @@
 import numpy as np
 import pandas as pd
 import requests
-from functools import reduce
 
 data1 = pd.read_html("/home/antpotapov2019/labs_for_py/Lab 3/problem 3/results_ejudge.html")
 data2 = pd.read_excel("/home/antpotapov2019/labs_for_py/Lab 3/problem 3/students_info.xlsx")
 
+take_smaller = lambda s1, s2: s1 if s1.sum() < s2.sum() else s2
+
 data2.rename(columns= {'login':'User'}, inplace=True)
 
+df.join(other.set_index('key'), on='key')
 
-data1.
-print(data1)
+print(data2)
 
 
 
